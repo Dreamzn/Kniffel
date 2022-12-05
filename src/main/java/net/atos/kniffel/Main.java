af16aadbf8c1734f17c6ea78d211b1a7b7a169d4
ananasuu <nina.friedrich02@gmail.com>
2022-12-05 16:37:24 +0100

    checks userinput

--- src/main/java/net/atos/kniffel/Main.java
+++ src/main/java/net/atos/kniffel/Main.java
@@ -10,17 +10,78 @@ import java.util.Random;
 
 public class Main {
 
-    protected static ArrayList<Integer> currentRoll = new ArrayList<Integer>();
-    protected static ArrayList<Integer> storedDice = new ArrayList<Integer>();
-    static Random rand = new Random();
+    private static ArrayList<Integer> currentRoll = new ArrayList<Integer>();
+    private static ArrayList<Integer> storedDice = new ArrayList<Integer>();
+    private static Random rand = new Random();
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * @param rolledDiced number of dice thrown
+     * @throws IOException in case the read operation from the command line fails
+     */
+    private static void chooseResult(int rolledDiced) throws IOException {
+        ArrayList<Integer> validateDice = new ArrayList<Integer>();
+
+        System.out.println("Welche Würfel möchtest du behalten? Trenne deine Eingabe durch Kommas.");                       //Input kept rolls
+        String answer = br.readLine();
+
+        if (!answer.equals("")) {
+            String[] choose = answer.split("\\D");
+            boolean validInput = false;
+            boolean validSelection = false;
+
+            String answerCheck = answer.replaceAll("\\W", "");                                              //Checks if Input is valid
+            char[] checkInput = answerCheck.toCharArray();
+            for (char c : checkInput) {
+                if (Character.isDigit(c)) {
+                    validInput = true;
+                } else {
+                    System.out.println("Das ist kein gültiges Zahlenformat.");
+                    validInput = false;
+                    break;
+                }
+            }
+
+            for (String s : choose) {
+                validateDice.add(Integer.valueOf(s));                                                                   //Adds chosen rolls to validation-list
+            }
+
+            if (currentRoll.containsAll(validateDice) && validInput) {                                                  //Checks if chosen rolls were actually rolled (anti-cheat)
+                validSelection = true;
+            } else if (!currentRoll.containsAll(validateDice) && validInput) {
+                System.out.println("Dieses Würfelergebnis wurde nicht gewürfelt. Bitte wähle erneut.");
+            }
 
 
-    private static List<Integer> chooseResult(String[] roll) {
-        return null;
+            // TODO: validate selection
+            // convert String array into list of integer values -> if none valid characters are detected validSelection has to be false
+            // check if the selected integers are part of the currentRoll list -> set valid selection to true
+
+
+            if (validSelection) {
+                for (String s : choose) {
+                    storedDice.add(Integer.valueOf(s));                                                                 //Add kept rolls to storedDice
+                }
+                currentRoll.clear();                                                                                    //Prevents cheating by limiting the selectable rolls to the current round of rolls
+            } else {
+                chooseResult(rolledDiced);
+                return;
+            }
+
+            System.out.println("Das sind " + choose.length + " von " + rolledDiced + " Würfeln.");
+            System.out.println("\nDeine aktuellen Würfel zeigen: " + storedDice + "\n");
+
+        } else if (storedDice.size() > 0) {                                                                             //zero rolls are kept
+            System.out.println("\nEs wurde keines der neuen Ergebnisse behalten." +
+                    " Deine vorherigen Ergebnisse sind: " + storedDice + "\n");
+        } else {
+            System.out.println("\nEs wurde keines der Ergebnisse behalten.\n");
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String[] choose = new String[0];
         int roll = 5;
         int result = 0;
@@ -38,36 +99,7 @@ public class Main {
             }
 
             if (reroll < 2) {
-                System.out.println("Welche Würfel möchtest du behalten? Trenne deine Eingabe durch Kommas.");              //Input kept rolls
-
-                String answer = br.readLine();
-                if (!answer.equals("")) {
-                    choose = answer.split("\\D");
-
-                    // regular for loop - Java Basic
-                    // for (int count = 0; count < choose.length; count++) {
-                    //     storedDice.add(Integer.valueOf(choose[count]));                                                    //Add kept rolls to storedDice
-                    // }
-
-                    // for each - Java 5
-                    for (String s : choose) {
-                        storedDice.add(Integer.valueOf(s));                                                                 //Add kept rolls to storedDice
-                    }
-
-                    // Streaming API - Java 8
-                    // Arrays.stream(choose).forEach( s -> storedDice.add(Integer.valueOf(s)));
-
-
-                    System.out.println("Das sind " + choose.length + " von " + roll + " Würfeln.");
-                    System.out.println("\nDeine aktuellen Würfel zeigen: " + storedDice + "\n");
-
-                } else if (storedDice.size() > 0) {                                                                        //zero rolls are kept
-                    System.out.println("\nEs wurde keines der neuen Ergebnisse behalten." +
-                            " Deine vorherigen Ergebnisse sind: " + storedDice + "\n");
-                } else {
-                    System.out.println("\nEs wurde keines der Ergebnisse behalten.\n");
-                }
-
+                chooseResult(roll);
             } else {                                                                                                       //Case: 3. round, need to choose enough rolls
                 System.out.println("\nDies ist die letzte Würfelrunde. Um auf 5 Ergebnisse zu kommen, werden alle Würfel übernommen.");
                 for (int getLast = 1; storedDice.size() < 5; getLast++) {
